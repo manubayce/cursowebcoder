e213a1d991624d0e5747b636d27ffc887cd1d699
ManuelBayce <manuelbayce24@gmail.com>
2021-05-02 20:39:54 -0300

    dsfdsfcommit253

--- navbar.js
+++ navbar.js
@@ -1,3 +1,20 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('.nav-menu').classList.toggle('show');
-});
+const filterItem = document.querySelector(".filter-item");
+const filterProducto = document.querySelectorAll(".div_productos");
+
+window.onload = ()=>{
+    filterItem.onclick = (selectedItem)=>{
+        if(selectedItem.target.classList.contains("filter-item")){
+            let filterName = selectedItem.target.getAttribute("data-name");
+            filterProducto.forEach((div_productos)=>{
+                let filterProducto = div_productos.getAttribute("data-name");
+                if((filterProducto == filterName) || filterName == "all"){
+                    div_productos.classList.remove("hide");
+                    div_productos.classList.add("show");
+                }else{
+                    div_productos.classList.remove("show");
+                    div_productos.classList.add("hide");
+                }
+            });
+        }
+    }
+}
