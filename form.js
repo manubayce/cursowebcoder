262f89e8929ec9f4e85477c8e577e679f3e9d93b
ManuelBayce <manuelbayce24@gmail.com>
2021-06-09 23:24:23 -0300

    Agregar objetos al proyecto

--- form.js
+++ form.js
@@ -1,12 +1,34 @@
-const nombre = document.getElementById("nombre")
-const apellido = document.getElementById("apellido")
-const telefono = document.getElementById("telefono")
-const email = document.getElementById("email")
-const form = document.getElementById("form")
+let nombre = document.getElementById("nombre")
+let apellido = document.getElementById("apellido")
+let telefono = document.getElementById("telefono")
+let email = document.getElementById("email")
+let form = document.getElementById("form")
 
 form.addEventListener("submit", e=>{
     e.preventDefault()
-    if(nombre.value.length <3 && apellido.value.length <3){
+    if(nombre.value.length <3 || apellido.value.length <3){
         alert("Este valor es demasiado corto")
     }
 })
+
+// Objeto
+
+let personaForm = {
+    Nombre = nombre,
+    Apellido = apellido,
+    Telefono = telefono,
+    Email = email,
+}
+
+console.log(personaForm);
+
+// Objeto 2
+
+let objeto2 = {
+    Nombre = "Manuel",
+    Apellido = "Bayce",
+    Telefono = "75889",
+    Email = "fdsfdsf@SpeechGrammarList.com",
+}
+
+console.log(objeto2);
