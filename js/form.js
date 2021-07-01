8f65c26da8db4b2a062796ee2381b18f14d3061e
ManuelBayce <manuelbayce24@gmail.com>
2021-06-30 23:58:21 -0300

    proyecto 2 sin terminar

--- js/form.js
+++ js/form.js
@@ -1,65 +1,75 @@
-let form = document.getElementById("form");
-let nombre = document.getElementById("nombre");
-let apellido = document.getElementById("apellido");
-let telefono = document.getElementById("telefono");
-let email = document.getElementById("email");
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#form input');
 
-form.addEventListener("submit", e=>{
-    e.preventDefault()
-    let mailvalidation = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    let enviar = false
-    if(nombre.value.length <3)
-    {
-        alert("El valor ingresado del nombre es demasiado corto.")
-        enviar = true
-    }
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{2,25}$/, // Letras y espacios, pueden llevar acentos
+	apellido: /^[a-zA-ZÀ-ÿ\s]{2,20}$/, // Letras y espacios, pueden llevar acentos
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
 
-    if(apellido.value.length <3 && enviar == false)
-    {
-        alert("El valor ingresado del apellido es demasiado corto.")
-        enviar = true
-    }
+const campos = {
+	nombre: false,
+    apellido: false,
+	correo: false,
+	telefono: false
+}
 
-    if(telefono.value.length <7 && enviar == false)
-    {
-        alert("El valor ingresado del teléfono es demasiado corto.")
-        enviar = true
-    }
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
 
-    if(!mailvalidation.test(email.value) && enviar == false) 
-    {
-        alert("El email ingresado no es válido.")
-        enviar = true
-    }
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "apellido":
+			validarCampo(expresiones.apellido, e.target, 'apellido');
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	if(campos.nombre && campos.apellido && campos.correo && campos.telefono && terminos.checked ){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
 })
-
-// Objeto
-
-// let personaForm = {    
-
-//     Nombre = nombre,    
-    
-//     Apellido = apellido,    
-    
-//     Telefono = telefono,    
-    
-//     Email = email
-    
-//     }
-
-// console.log(personaForm);
-
-// Array
-
-// let personasFormulario = [
-//     {nombre: "manuel", apellido: "bayce", telefono: 456980, email: "manuelbayce@gmail.com"},
-//     {nombre: "Eduardo", apellido: "lopez", telefono: 693012, email: "eduardolopez@gmail.com"},
-//     {nombre: "sofía", apellido: "Alvarez", telefono: 421367, email: "sofiaalvarez@gmail.com"},
-//     {nombre: "mariana", apellido: "Gutierrez", telefono: 124368, email: "marianaalvarez@gmail.com"},
-// ]
-
-// let nombreMayus = personasFormulario.map(nombre => nombre.toUpperCase())
-// let apellidoMayus = personasFormulario.map(apellido => apellido.toUpperCase())
-
-// console.log(nombreMayus);
-// console.log(apellidoMayus);
